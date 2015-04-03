26126873e2f877e2965c96309c69de2d2522627d
Karthik <mkarthik@me.com>
2015-04-02 22:21:51 -0700

    Added animate on scroll
    + wide bug fixes, code re-organisation

--- scripts.js
+++ scripts.js
@@ -16,17 +16,18 @@ window.onload = function () {
 
 //Scroll events
 window.onscroll = function () {
+/*
 	var brightness = (window.pageYOffset/2 > 100) ? 100 : window.pageYOffset/2;
 	var level = brightness < 20 ? 20 : brightness;
 	var hello = document.getElementsByClassName("pre-hello")[0];
 	hello.setAttribute("style", '-webkit-filter: brightness('+level+'%)');
-	hello.setAttribute("style", 'filter: brightness('+level+'%)');
+*/
 		
 	//Disable parallax for Mobile devices
 	if(typeof window.orientation == 'undefined') {		
 		var parallax = document.querySelectorAll(".pre");
 		[].slice.call(parallax).forEach(function(el,i) {
-			var bgPosition = "50% " + (window.pageYOffset/8 * 0.2) + "px";
+			var bgPosition = "50% " + (window.pageYOffset/-2) + "px";
 			el.style.backgroundPosition = bgPosition;
 		});	
 	}
@@ -40,7 +41,7 @@ window.onscroll = function () {
 	//Initial shadow over main container
 	if (document.body.scrollTop > 0) {	
 		$('.mkcontainer').css({
-            'box-shadow': '2px 0px 30px 10px rgba(100, 100, 100, 0.5)'
+            'box-shadow': '2px 0px 30px 10px rgba(100, 100, 100, 0.3)'
         });
 	}
 	else {
@@ -49,15 +50,15 @@ window.onscroll = function () {
 		});
 	}
 
-	
 	//Make navbar smaller
-    if (document.body.scrollTop > document.getElementsByClassName('mkcontainer')[0].offsetTop - 25) {
+	var dist = document.getElementsByClassName('mkcontainer')[0].offsetTop - 25
+    if (document.body.scrollTop > dist || document.documentElement.scrollTop > dist) {
         $('.logo').removeClass("slideInDown");
         $('.header').css({
             'margin-top': '-20px',
             'z-index': '1000',
             'background-color': 'rgba(255, 255, 120, 0.95)',
-            'box-shadow': '0px 0px 15px 10px rgba(100, 100, 100, 0.3)'
+            'box-shadow': '0px 0px 15px 10px rgba(100, 100, 100, 0.2)'
         });
         $('.logo').addClass("fadeOutUp");
 
