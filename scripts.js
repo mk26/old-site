0db8eb4eff49e67d95c71b6ac986393f89c71c26
Karthik <mkarthik@me.com>
2015-04-03 18:34:15 -0700

    Added project logos
    + hello image change

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ window.onload = function () {
 	//Go to section if present in URL
     if (window.location.hash) 
     	smoothScroll(window.location);
+    $('#top-right-logo').hide();
 };
 
 //Scroll events
@@ -22,7 +23,13 @@ window.onscroll = function () {
 	var hello = document.getElementsByClassName("pre-hello")[0];
 	hello.setAttribute("style", '-webkit-filter: brightness('+level+'%)');
 */
-		
+/*
+	var scaleValue = window.pageYOffset/100 < 1 ? 1 : window.pageYOffset/100;
+	scaleValue = scaleValue > 4 ? 4 : scaleValue;
+	$('.pre-hello').css({
+		'-webkit-transform': 'scale('+scaleValue+')'
+	});
+*/
 	//Disable parallax for Mobile devices
 	if(typeof window.orientation == 'undefined') {		
 		var parallax = document.querySelectorAll(".pre");
@@ -34,7 +41,7 @@ window.onscroll = function () {
 	else {
 		$('.pre').css({
 			'background-attachment': 'scroll',
-			'background-position': '0% 0%'
+			'background-position': '50% 0%'
 		});
 	}
 	
@@ -50,19 +57,18 @@ window.onscroll = function () {
 		});
 	}
 
-	//Make navbar smaller
-	var dist = document.getElementsByClassName('mkcontainer')[0].offsetTop - 25
+	//Navbar adjustments
+	var dist = document.getElementsByClassName('mkcontainer')[0].offsetTop - 25;
+	var dist2 = document.getElementsByClassName('pre-proj')[0].offsetTop + 25;
     if (document.body.scrollTop > dist || document.documentElement.scrollTop > dist) {
         $('.logo').removeClass("slideInDown");
-        $('#top-right-logo').removeClass("rotateOutUpRight");
         $('.header').css({
             'margin-top': '-20px',
             'z-index': '1000',
-            'background-color': 'rgba(255, 255, 120, 0.95)',
+            'background-color': 'rgba(255, 255, 255, 0.9)',
             'box-shadow': '0px 0px 15px 10px rgba(100, 100, 100, 0.2)'
         });
         $('.logo').addClass("fadeOutUp");
-		$('#top-right-logo').addClass("rotateInDownRight");
     } else {
         $('.logo').removeClass("fadeOutUp");
 		$('#top-right-logo').removeClass("rotateInDownRight");
@@ -74,6 +80,16 @@ window.onscroll = function () {
         });
 		$('#top-right-logo').addClass("rotateOutUpRight");
     }
+	if (document.body.scrollTop > dist2 || document.documentElement.scrollTop > dist2) {
+		$('#top-right-logo').show().removeClass("rotateOutUpRight");
+			$('.header').css({
+            'margin-top': '-20px',
+            'z-index': '1000',
+            'background-color': 'rgba(255, 255, 120, 0.9)',
+            'box-shadow': '0px 0px 15px 10px rgba(100, 100, 100, 0.2)'
+        });
+		$('#top-right-logo').addClass("rotateInDownRight");
+	}
 };
 
 //Double click navbar to go to top
